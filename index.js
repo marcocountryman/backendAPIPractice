0501d8d4ee9dc4717c7db6a246215f11cd5db21a
mcountryman <countryman.marco@gmail.com>
2022-03-11 14:12:55 -0500

    post and delete route working

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import bodyParser from 'body-parser' //<----- Can take in post request bodys
 
+import usersRoutes from './routes/users.js';
+
 const app = express(); // <--application lives in this;
 const PORT = 4000; //Can be any number
 
 app.use(bodyParser.json());//JSON data being used in entire application
+
+app.use('/users', usersRoutes);
+
+app.get('/', (req, res) => res.send('Hello from Homepage'));
 
 app.listen(PORT, () => console.log(`Serving running on port: http://localhost:${PORT}`));
 //listening to incoming requests
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,36 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
+const users = [
+
+]
 
 // all routes are already starting with /users!!!!!
-router.get('/users', (req,res) => {
-    res.send('User Route')
+router.get('/', (req,res) => {
+    res.send(users);
+})
+
+router.post('/', (req, res) => {
+    const user = req.body;
+    const userId = uuidv4();
+    const userWithId = { ...user, id: userId };
+    users.push(userWithId);
+
+    res.send(`User with the name ${user.firstName} added to the Database`);
+})
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const user = users.find(user => user.id === id);
+    res.send(user);
+})
+
+router.delete('/:id', (req,res) => {
+    const id = req.params.id;
+
+    const newUsers = users.filter(user => user.id !== id);
+    res.send(newUsers);
 })
 export default router;
